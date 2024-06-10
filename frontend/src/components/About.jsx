cd849aa9fa0f92c490db588d5c4b83fdf0b8e9f6
Ganesh Sharma <ganesh220503@gmail.com>
2024-06-10 10:04:22 +0530

    adding files to github

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HiReply } from 'react-icons/hi'
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Event</h4>
+      <h2>ABOUT</h2>
+      <p>Welcome to <b>EVENT</b>, where we specialize in crafting unforgettable events, from weddings to birthday celebrations. With 15 years of experience, our team is dedicated to turning your vision into reality with meticulous attention to detail and personalized service. We pride ourselves on our creativity, professionalism, and ability to exceed expectations at every turn. From venue selection to décor, catering, and entertainment, we ensure that every aspect of your event reflects your unique style and personality. Contact us today to begin planning your next special occasion and let us create memories that will last a lifetime.</p>
+    </section>
+  )
+}
+
+export default About
+
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+      <div className="banner">
+        <div className="title">
+          <h1>KING's</h1>
+          <p>Events And Weddings</p>
+        </div>
+        <div className="tag">
+          <label>News Letter</label>
+          <div>
+            <input type="text" />
+            <button>Subscribe</button>
+          </div>
+          <p>Sign up with your email to recieve news and update</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default footer
